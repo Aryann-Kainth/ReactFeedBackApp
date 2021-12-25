b617343bf0a1aca0fab77b54fe3fbd0a2ab8c201
aryan <kaintharyan@gmail.com>
2021-12-25 11:17:19 +0530

    global states

--- src/App.js
+++ src/App.js
@@ -27,15 +27,21 @@
 
 }*/
 // above is basics of JSX 
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedBackItem from "./components/FeedBackItem";
+import FeedbackData from "./data/FeedBackData";
+import FeedbackList from "./components/FeedbackList";
 function App(){
+  const [feedback,setFeedback]=useState(FeedbackData)
 return (
   <>
   <Header /> 
   <div className='container'>
-<h1>My App</h1>
+  <FeedbackList feedback={feedback}/>
 
   </div>
+ 
   </>
 )  
 }
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import FeedBackItem from './FeedBackItem'
+function FeedbackList(props) {
+   // console.log(props.feedback)
+   if(!props.feedback||props.feedback.length==0)
+   {
+       return <p>Nothing to show</p>
+   }
+    return (
+        <div className='feedback-list'>
+           <ul>
+          {props.feedback.map((comment,index)=>(
+           <FeedBackItem key={comment.id} item={comment} />
+            ))}
+          </ul> 
+        </div>
+    )
+}
+
+export default FeedbackList
