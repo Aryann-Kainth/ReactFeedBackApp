cab5cd0806bf146cff84769b0627ddeeaa893f1c
aryan <kaintharyan@gmail.com>
2022-01-07 16:51:11 +0530

    Fade Animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedBackItem from './FeedBackItem'
+import {motion,AnimatePresence} from 'framer-motion'
 function FeedbackList(props) {
    // console.log(props.feedback)
    if(!props.feedback||props.feedback.length==0)
@@ -8,11 +9,18 @@ function FeedbackList(props) {
    }
     return (
         <div className='feedback-list'>
-           <ul>
+            <AnimatePresence>
+            <ul>
           {props.feedback.map((comment,index)=>(
+              <motion.div key={comment.id} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+
+             
            <FeedBackItem key={comment.id} item={comment} handleDelete={props.handleDelete} />
+           </motion.div>
             ))}
           </ul> 
+            </AnimatePresence>
+           
         </div>
     )
 }
