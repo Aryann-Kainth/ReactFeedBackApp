0aaffd98d236512e898bd0038daf1dbb3cfce14c
aryan <kaintharyan@gmail.com>
2022-01-07 15:22:58 +0530

    real time validation on input box

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
-import {useState} from 'react'
+import { useState } from 'react'
 function FeedbackForm() {
-    const [text,setText]=useState('')
-    const  handleTextChange=(e)=>{
-       // console.log(e.target.value);
-       setText(e.target.value);
+    const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+    const handleTextChange = (e) => {
+        // console.log(e.target.value);
+        if (text == '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        }
+        else if (text !== '' && text.trim().length < 10) {
+
+            setBtnDisabled(true)
+            setMessage("Text must have atleast 10 characters")
+        }
+        else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
+        setText(e.target.value);
     }
     return (
         <Card>
             <form >
                 <h2>Rate your Experience</h2>
                 <div className="input-group">
-                    <input onChange={handleTextChange} type="text" placeholder='write a review' value={text} / >
-                       <Button type='submit'  >Add a Review</Button> 
+                    <input onChange={handleTextChange} type="text" placeholder='write a review' value={text} />
+                    <Button type='submit' isDisabled={btnDisabled} >Add a Review</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
